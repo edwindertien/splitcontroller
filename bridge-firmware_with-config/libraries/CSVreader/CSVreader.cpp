adbe2ffc30743d1fbdd42cf1ad1ca19d599cc384
Lonn <lonn.wolf031@gmail.com>
2021-04-23 17:18:39 +0200

    update

--- bridge-firmware_with-config/libraries/CSVreader/CSVreader.cpp
+++ bridge-firmware_with-config/libraries/CSVreader/CSVreader.cpp
@@ -9,39 +9,26 @@
 class CSVIterator
 {
     public:
-        typedef std::input_iterator_tag     iterator_category;
-        typedef CSVRow                      value_type;
-        typedef std::size_t                 difference_type;
-        typedef CSVRow*                     pointer;
-        typedef CSVRow&                     reference;
 
-        CSVIterator(std::istream& str)  :m_str(str.good()?&str:NULL) { ++(*this); }
-        CSVIterator()                   :m_str(NULL) {}
+        CSVIterator::CSVIterator(std::istream& str)  :m_str(str.good()?&str:NULL) { ++(*this); }
+        CSVIterator::CSVIterator()                   :m_str(NULL) {}
 
         // Pre Increment
-        CSVIterator& operator++()               {if (m_str) { if (!((*m_str) >> m_row)){m_str = NULL;}}return *this;}
+        CSVIterator::CSVIterator& operator++()               {if (m_str) { if (!((*m_str) >> m_row)){m_str = NULL;}}return *this;}
         // Post increment
-        CSVIterator operator++(int)             {CSVIterator    tmp(*this);++(*this);return tmp;}
-        CSVRow const& operator*()   const       {return m_row;}
-        CSVRow const* operator->()  const       {return &m_row;}
+        CSVIterator::CSVIterator operator++(int)             {CSVIterator    tmp(*this);++(*this);return tmp;}
+        CSVIterator::CSVRow const& operator*()   const       {return m_row;}
+        CSVIterator::CSVRow const* operator->()  const       {return &m_row;}
 
         bool operator==(CSVIterator const& rhs) {return ((this == &rhs) || ((this->m_str == NULL) && (rhs.m_str == NULL)));}
         bool operator!=(CSVIterator const& rhs) {return !((*this) == rhs);}
-    private:
-        std::istream*       m_str;
-        CSVRow              m_row;
 };
 
-class CSVRange
-{
-    std::istream&   stream;
-    public:
-        CSVRange(std::istream& str)
-            : stream(str)
-        {}
-        CSVIterator begin() const {return CSVIterator{stream};}
-        CSVIterator end()   const {return CSVIterator{};}
-};
+
+
+CSVRange::CSVIterator begin() const {return CSVIterator{stream};}
+CSVRange::CSVIterator end()   const {return CSVIterator{};}
+
 
 int main()
 {
--- bridge-firmware_with-config/libraries/CSVreader/CSVreader.h
+++ bridge-firmware_with-config/libraries/CSVreader/CSVreader.h
@@ -13,19 +13,6 @@ class CSVreader {
       typedef std::size_t                 difference_type;
       typedef CSVRow*                     pointer;
       typedef CSVRow&                     reference;
-
-      CSVIterator(std::istream& str)  :m_str(str.good()?&str:NULL) { ++(*this); }
-      CSVIterator()                   :m_str(NULL) {}
-
-      // Pre Increment
-      CSVIterator& operator++()               {if (m_str) { if (!((*m_str) >> m_row)){m_str = NULL;}}return *this;}
-      // Post increment
-      CSVIterator operator++(int)             {CSVIterator    tmp(*this);++(*this);return tmp;}
-      CSVRow const& operator*()   const       {return m_row;}
-      CSVRow const* operator->()  const       {return &m_row;}
-
-      bool operator==(CSVIterator const& rhs) {return ((this == &rhs) || ((this->m_str == NULL) && (rhs.m_str == NULL)));}
-      bool operator!=(CSVIterator const& rhs) {return !((*this) == rhs);}
   private:
       std::istream*       m_str;
       CSVRow              m_row;
@@ -34,10 +21,4 @@ class CSVreader {
 class CSVRange
 {
     std::istream&   stream;
-    public:
-        CSVRange(std::istream& str)
-            : stream(str)
-        {}
-        CSVIterator begin() const {return CSVIterator{stream};}
-        CSVIterator end()   const {return CSVIterator{};}
 };
