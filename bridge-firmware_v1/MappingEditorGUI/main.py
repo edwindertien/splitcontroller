7203c85b3f103a253afce7594b854db0a5fd8fcd
lonnwolf031 <lonn.wolf031@gmail.com>
2021-09-15 14:32:49 +0200

    Update main.py

--- bridge-firmware_v1/MappingEditorGUI/main.py
+++ bridge-firmware_v1/MappingEditorGUI/main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication, QDialog, QGridLayout, QLabel, QLineEdit, QFileDialog, QPushButton, QComboBox
+import csv
+import pandas
+from PyQt5.QtWidgets import QAction, QWidget, QApplication, QDialog, QGridLayout, QLabel, QLineEdit, QFileDialog, QScrollArea, QPushButton, QComboBox
 from PyQt5.Qt import QHBoxLayout, QWindow, QMainWindow, QVBoxLayout
 
 ps4btns = ["joyRXax2", "joyRYax1", "joyRZb11", "joyLZb12", "joyLYax4", "joyLXax3", "sqbutton", "circbutton", "psbutton", "l2button", "r2button", "tributton", "xbutton", "lhat", "rhat", "uhat", "dhat", "r1button6", "l1button5", "optbutton", "accx1", "accy1", "accx2", "accy2", "c_button1", "z_button1", "c_button2", "z_button2", "shrebutton", "PWM1", "PWM2", "PWM3", "PWM4", "PWM5"]
@@ -16,61 +18,82 @@ class MainWindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        vlayoutmain = QVBoxLayout()
-        hlayoutsub1 = QHBoxLayout()
-        hlayoutsub2 = QHBoxLayout()
-        hlayoutsub3 = QHBoxLayout()
-        hlayoutsub4 = QHBoxLayout()
-        widget = QWidget()
-        widget.setLayout(vlayoutmain)
+        self.vlayoutmain = QVBoxLayout()
+        self.hlayoutsub1 = QHBoxLayout()
+        self.hlayoutsub2 = QHBoxLayout()
+        self.hlayoutsub3 = QHBoxLayout()
+        self.hlayoutsub4 = QHBoxLayout()
+        self.widget = QWidget()
+        self.widget.setLayout(self.vlayoutmain)
         # setup of top layout
-        label1 = QLabel('Please choose a file or start from scratch')
-        hlayoutsub1.addWidget(label1)
+        self.label1 = QLabel('Please choose a file or start from scratch')
+        self.hlayoutsub1.addWidget(self.label1)
         # next row
-        btnChooseFile = QPushButton("Open File")
-        btnChooseFile.clicked.connect(self.open)  # connect clicked to self.open()
-        hlayoutsub2.addWidget(btnChooseFile)
+        self.btnChooseFile = QPushButton("Open File")
+        self.btnChooseFile.clicked.connect(self.open)  # connect clicked to self.open()
+        self.hlayoutsub2.addWidget(self.btnChooseFile)
         # next row
-        label2 = QLabel('Choose a menu to edit or make a new one')
-        hlayoutsub3.addWidget(label2)
+        self.label2 = QLabel('Choose a menu to edit or make a new one')
+        self.hlayoutsub3.addWidget(self.label2)
         # next row
-        chooseMenuCombo = QComboBox()
+        self.chooseMenuCombo = QComboBox()
         for thing in range(len(menus)):
-            chooseMenuCombo.addItem(thing)
-        hlayoutsub4.addWidget(chooseMenuCombo)
-        chosenButton = QPushButton("Edit this menu")
-        chosenButton.clicked.connect(self.choice)
+            self.chooseMenuCombo.addItem(thing)
+        self.hlayoutsub4.addWidget(self.chooseMenuCombo)
+        self.chosenButton = QPushButton("Edit this menu")
+        self.chosenButton.clicked.connect(self.choice)
+        self.hlayoutsub4.addWidget(self.chosenButton)
+        self.txtMenu = QLineEdit()
+        self.hlayoutsub4.addWidget(self.txtMenu)
+        self.makeButton = QPushButton("Make this menu")
+        self.makeButton.clicked.connect(self.make)
+        self.hlayoutsub4.addWidget(self.makeButton)
 
-        # make grid
-        gridlayout = QGridLayout()
-        gridlayout.setColumnStretch(1, 4)
-        gridlayout.setColumnStretch(2, 4)
-        gridlayout.addWidget(QPushButton('1'), 0, 0)
-        gridlayout.addWidget(QPushButton('2'), 0, 1)
-        gridlayout.addWidget(QPushButton('3'), 0, 2)
-        gridlayout.addWidget(QPushButton('4'), 1, 0)
-        gridlayout.addWidget(QPushButton('5'), 1, 1)
-        gridlayout.addWidget(QPushButton('6'), 1, 2)
-        gridlayout.addWidget(QPushButton('7'), 2, 0)
-        gridlayout.addWidget(QPushButton('8'), 2, 1)
-        gridlayout.addWidget(QPushButton('9'), 2, 2)
+        # make grid in scrollarea
+        self.hlayoutsub5 = QHBoxLayout()
+        self.scrollArea = QScrollArea()
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QWidget()
+        self.gridlayout = QGridLayout(self.scrollAreaWidgetContents)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.hlayoutsub5.addWidget(self.scrollArea)
+        self.gridlayout.setColumnStretch(1, 4)
+        self.gridlayout.setColumnStretch(2, 4)
+        self.gridlayout.addWidget(QPushButton('1'), 0, 0)
+        self.gridlayout.addWidget(QPushButton('2'), 0, 1)
+        self.gridlayout.addWidget(QPushButton('3'), 0, 2)
+        self.gridlayout.addWidget(QPushButton('4'), 1, 0)
+        self.gridlayout.addWidget(QPushButton('5'), 1, 1)
+        self.gridlayout.addWidget(QPushButton('6'), 1, 2)
+        self.gridlayout.addWidget(QPushButton('7'), 2, 0)
+        self.gridlayout.addWidget(QPushButton('8'), 2, 1)
+        self.gridlayout.addWidget(QPushButton('9'), 2, 2)
 
         # next
 
-        hlayoutsub1.addStretch(2)
+        self.hlayoutsub1.addStretch(2)
 
-        hlayoutsub2.addStretch(1)
+        self.hlayoutsub2.addStretch(1)
+
+        # for save file later
+        saveFile = QAction("&Save File", self)
+        saveFile.setShortcut("Ctrl+S")
+        saveFile.setStatusTip('Save File')
+        saveFile.triggered.connect(self.file_save)
+
+        # finally
 
 
-        vlayoutmain.addLayout(hlayoutsub1)
-        vlayoutmain.addLayout(hlayoutsub2)
-        vlayoutmain.addLayout(hlayoutsub3)
-        vlayoutmain.addLayout(gridlayout)
+        self.vlayoutmain.addLayout(self.hlayoutsub1)
+        self.vlayoutmain.addLayout(self.hlayoutsub2)
+        self.vlayoutmain.addLayout(self.hlayoutsub3)
+        self.vlayoutmain.addLayout(self.hlayoutsub4)
+        self.vlayoutmain.addLayout(self.hlayoutsub5)
 
 
-        vlayoutmain.addStretch(1)
+        self.vlayoutmain.addStretch(1)
 
-        self.setCentralWidget(widget)
+        self.setCentralWidget(self.widget)
 
         self.setGeometry(500, 500, 500, 500)
         self.setWindowTitle('Edit splitcontroller configuration')
@@ -86,11 +109,36 @@ class MainWindow(QMainWindow):
         list = []
         return list
 
+    def make(self):
+        pass
+
+    def file_save(self):
+        name = QtGui.QFileDialog.getSaveFileName(self, 'Save File')
+        file = open(name,'w')
+        text = self.textEdit.toPlainText()
+        file.write(text)
+        file.close()
+
+class DataHandler():
+    def __init__():
+        pass
+
+    def readCSV(path):
+        try:
+            # change 'Name' with first column name, so first word in csv
+            df = pandas.read_csv(path, index_col='Name')
+            print(df)
+        except:
+            msg = "Failed to read CSV"
+        finally:
+            pass
+    def writeCSV(df):
+        df.to_csv('mapping_modified.csv')
+
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     ex = MainWindow()
-#     ex.show()
     sys.exit(app.exec_())
 
 
